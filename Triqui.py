be82cf27029e9f511a51442bf76a0ad1e37d8da7
Juan Nicolás Barreto Parra <jnbarretop@correo.udistrital.edu.co>
2022-07-08 17:40:47 -0500

    Nuevo commit

---
+++ Triqui.py
@@ -0,0 +1,32 @@
+from CVerificador import Verificador;
+
+
+matriz=[
+    [1,2,3],    
+    [4,5,6],
+    ['X','X','X']
+]
+
+
+miVerificador= Verificador();
+print(miVerificador.verificarFilas(matriz))
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
